b8f1e209bbb0b4d8728c84ab3a7e8aab221acd21
Alex Fleming <alex@alexfleming.dev>
2020-06-09 23:24:02 +0100

    Move to a common checkErr function, move to using APIGatewayProxyRequest and APIGatewayProxyResponse

--- cmd/persist-post.go
+++ cmd/persist-post.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -13,22 +14,22 @@ import (
 )
 
 type Post struct {
-	UserId string `json:"userId" dynamodbav:"UserId"`
-	Message string `json:"message" dynamodbav:"Message"`
+	UserId           string    `json:"userId" dynamodbav:"UserId"`
+	Message          string    `json:"message" dynamodbav:"Message"`
 	CreatedTimestamp time.Time `json:"createdTimestamp" dynamodbav:"CreatedTimestamp"`
 	UpdatedTimestamp time.Time `json:"updatedTimestamp" dynamodbav:"UpdatedTimestamp"`
 }
 
-func handle(_ context.Context, post Post) (string, error) {
+func handle(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	sess := session.Must(session.NewSession())
 	svc := dynamodb.New(sess)
 
+	var post Post
+	err := json.Unmarshal([]byte(req.Body), &post)
+	checkErr(err, "Couldn't unmarshall JSON")
+
 	av, err := dynamodbattribute.MarshalMap(post)
-	if err != nil {
-		fmt.Println("Couldn't marshall post:")
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
+	checkErr(err, "Couldn't marshall post")
 
 	input := &dynamodb.PutItemInput{
 		Item:      av,
@@ -36,12 +37,19 @@ func handle(_ context.Context, post Post) (string, error) {
 	}
 
 	avOut, err := svc.PutItem(input)
+	checkErr(err, "Could not PutItem")
+
+	jsonOut, err := json.Marshal(avOut)
+	checkErr(err, "Error marshalling output to json")
+	return events.APIGatewayProxyResponse{StatusCode: 200, Body: string(jsonOut)}, nil
+}
+
+func checkErr(err error, msg string) {
 	if err != nil {
-		fmt.Println("Could not PutItem:")
+		fmt.Println(msg + ": ")
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	return avOut.String(), nil
 }
 
 func main() {
