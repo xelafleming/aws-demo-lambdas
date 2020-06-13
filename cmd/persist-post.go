08aeecec2114481839225440dc430c7db7dd32af
Alex Fleming <alex@alexfleming.dev>
2020-06-13 20:54:55 +0100

    Change to a MessageId

--- cmd/persist-post.go
+++ cmd/persist-post.go
@@ -16,7 +16,7 @@ import (
 
 type Post struct {
 	UserId           string    `dynamodbav:"UserId"`
-	PostId           string    `dynamodbav:"MessageId"`
+	MessageId        string    `dynamodbav:"MessageId"`
 	Message          string    `dynamodbav:"Message"`
 	CreatedTimestamp time.Time `dynamodbav:"CreatedTimestamp"`
 	UpdatedTimestamp time.Time `dynamodbav:"UpdatedTimestamp"`
@@ -38,7 +38,7 @@ func handle(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, 
 
 	post := Post{
 		UserId:           username,
-		PostId:           uuid.New().String(),
+		MessageId:        uuid.New().String(),
 		Message:          message.Message,
 		CreatedTimestamp: time.Now(),
 		UpdatedTimestamp: time.Now(),
