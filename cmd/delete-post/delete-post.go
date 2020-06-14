98b99c6d6dac55d5c4434e04fed6a55222d90cfc
Alex Fleming <alex@alexfleming.dev>
2020-06-14 22:25:12 +0100

    delete-post.go implementation

---
+++ cmd/delete-post/delete-post.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"aws-demo-lambdas/internal/model"
+	"aws-demo-lambdas/internal/util"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	svc := util.InitDynamoConnection()
+	username := util.GetUsername(req)
+
+	var post model.Post
+	err := json.Unmarshal([]byte(req.Body), &post)
+	if err != nil {
+		fmt.Println("Couldn't unmarshal request:")
+		fmt.Println(err.Error())
+		return events.APIGatewayProxyResponse{StatusCode: 500}, errors.New("couldn't read request")
+	}
+
+	_, err = svc.DeleteItem(&dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"UserId": {
+				S: aws.String(username),
+			},
+			"MessageId": {
+				S: aws.String(post.MessageId),
+			},
+		},
+		TableName: aws.String("posts"),
+	})
+	if err != nil {
+		fmt.Println("Couldn't delete item from posts:")
+		fmt.Println(err.Error())
+		return events.APIGatewayProxyResponse{StatusCode: 500}, errors.New("couldn't delete post")
+	}
+
+	return events.APIGatewayProxyResponse{StatusCode: 204}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
